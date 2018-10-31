ea7dc0d555037201dc2fdeee393192c7a8590091
Iggys4ur <eswaind@gmail.com>
2018-10-30 23:17:12 -0400

    Version 1.1
    - plugin class now acting as singleton
    - code polished

--- src/site/modicum/rgrp/RGRP.java
+++ src/site/modicum/rgrp/RGRP.java
@@ -5,7 +5,7 @@ package site.modicum.rgrp;
 AUTHOR: IGGYSAUR
 DATE: 26/10/2018
 FOR: RETRIBUTION GAMING - WILD WEST FRONTIER
-VERSION 1.0
+VERSION 1.1
 
  */
 
@@ -17,42 +17,40 @@ import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
 public class RGRP extends JavaPlugin implements Listener, CommandExecutor {
 
+    private double costPerRail = getConfig().getDouble("costPerRail");
+    private String currency = getConfig().getString("currency");
     private SQLiteLib sqlLib;
-    private FileConfiguration config = getConfig();
-    private double costPerRail = config.getDouble("costPerRail");
-    private String currency = config.getString("currency");
     private List<Integer> blockList;
-    private RGRP plugin = this;
     private Database railsDB;
+
+    private static RGRP INSTANCE;
 
     @Override
     public void onEnable()
     {
-        sqlLib = SQLiteLib.hookSQLiteLib(plugin);
+        INSTANCE = this;
+        sqlLib = SQLiteLib.hookSQLiteLib(INSTANCE);
         sqlLib.initializeDatabase("protectedRails", "CREATE TABLE IF NOT EXISTS rails(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, rail BLOB, block BLOB, town BLOB, player BLOB)");
         railsDB = sqlLib.getDatabase("protectedRails");
 
-        // Register our command "kit" (set an instance of your command class as executor)
-        this.getCommand("rgrp").setExecutor(new cmdRGRP(this));
+        getCommand("rgrp").setExecutor(new cmdRGRP());
 
 
-        config.options().copyDefaults(true);
+        getConfig().options().copyDefaults(true);
         saveDefaultConfig();
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(INSTANCE, INSTANCE);
     }
 
     @EventHandler
@@ -163,23 +161,29 @@ public class RGRP extends JavaPlugin implements Listener, CommandExecutor {
 
     protected boolean isProtectedBlock(int itemInHand)
     {
-        return config.getIntegerList("blockIDtoProtect").contains(itemInHand);
+        return getConfig().getIntegerList("blockIDtoProtect").contains(itemInHand);
     }
 
-    protected void addToBlockList(int blockIDtoAdd)
+    protected boolean addToBlockList(int blockID)
     {
-        blockList = config.getIntegerList("blockIDtoProtect");
-        blockList.add(blockIDtoAdd);
-        config.set("blockIDtoProtect", blockList);
+        if(!isProtectedBlock(blockID)){
+        blockList = getConfig().getIntegerList("blockIDtoProtect");
+        blockList.add(blockID);
+        getConfig().set("blockIDtoProtect", blockList);
         saveConfig();
+        return true;
+        }else return false;
     }
 
-    protected void removeFromBlockList(int blockIDtoRemove)
+    protected boolean removeFromBlockList(int blockID)
     {
-        blockList = config.getIntegerList("blockIDtoProtect");
-        blockList.remove(new Integer(blockIDtoRemove));
-        config.set("blockIDtoProtect", blockList);
-        saveConfig();
+        if(isProtectedBlock(blockID)){
+            blockList = getConfig().getIntegerList("blockIDtoProtect");
+            blockList.remove(new Integer((blockID)));
+            getConfig().set("blockIDtoProtect", blockList);
+            saveConfig();
+            return true;
+        }else return false;
     }
 
     private boolean isOwned(Location location)
@@ -215,6 +219,11 @@ public class RGRP extends JavaPlugin implements Listener, CommandExecutor {
     private String deleteStatement(String column, Object queryToDelete)
     {
         return "DELETE FROM `rails` WHERE " + column + " = '" + queryToDelete + "';";
+    }
+
+    protected static RGRP getInstance()
+    {
+        return INSTANCE;
     }
 
     @Override
--- src/site/modicum/rgrp/cmdRGRP.java
+++ src/site/modicum/rgrp/cmdRGRP.java
@@ -8,54 +8,53 @@ import org.bukkit.entity.Player;
 
 public class cmdRGRP implements CommandExecutor {
 
-    private RGRP plugin;
-
-    protected cmdRGRP(RGRP plg) {
-        plugin = plg;
-    }
+    private RGRP plugin = RGRP.getInstance();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (!cmd.getName().equalsIgnoreCase("rgrp") || args.length != 1 || !(sender instanceof Player) || !sender.hasPermission("rgrp.admin")){ return false; }
+        if (args.length != 1 || !(sender instanceof Player) || !sender.hasPermission("rgbf.admin")) { return false; }
 
         Player player = (Player) sender;
         int blockID = player.getItemInHand().getTypeId();
+        char arg = args[0].toLowerCase().charAt(0);
 
-        if (args[0].toLowerCase().charAt(0) == 'a') //add
-        {
-            if (!plugin.isProtectedBlock(blockID)) {
-                plugin.addToBlockList(blockID);
-                player.sendMessage("");
-                player.sendMessage("§a[RGRP] §fBlockID: §3" + blockID + "§2 added");
-                player.sendMessage("§e       - Config list updated.");
-            } else {
-                player.sendMessage("");
-                player.sendMessage("§a[RGRP] §fBlockID: §3" + blockID + "§2 is already registered §fon the list.");
-            }
+        switch (arg) {
+
+            default: return false;
+
+            case 'a':
+                if (plugin.addToBlockList(blockID)) {
+                    player.sendMessage("");
+                    player.sendMessage("§a[RGBF] §fBlockID: §3" + blockID + "§2 added");
+                    player.sendMessage("§e       - Config list updated.");
+                } else {
+                    player.sendMessage("");
+                    player.sendMessage("§a[RGBF] §fBlockID: §3" + blockID + "§2 is already registered §fon the list.");
+                }
+                break;
+
+            case 'r':
+                if (plugin.removeFromBlockList(blockID)) {
+                    player.sendMessage("");
+                    player.sendMessage("§a[RGBF] §fBlockID: §3" + blockID + "§4 removed");
+                    player.sendMessage("§e       - Config list updated.");
+                } else {
+                    player.sendMessage("");
+                    player.sendMessage("§a[RGBF] §fBlockID: §3" + blockID + "§4 is *NOT* registered §fon the list.");
+                }
+                break;
+
+            case 'c':
+                if (plugin.isProtectedBlock(blockID)) {
+                    player.sendMessage("");
+                    player.sendMessage("§a[RGBF] §fBlockID: §3" + blockID + "§2 is registered §fon the list.");
+                } else {
+                    player.sendMessage("");
+                    player.sendMessage("§a[RGBF] §fBlockID: §3" + blockID + "§4 is *NOT* registered §fon the list.");
+                }
+                break;
         }
-        else if (args[0].toLowerCase().charAt(0) == 'r') // remove
-        {
-            if (plugin.isProtectedBlock(blockID)) {
-                plugin.removeFromBlockList(blockID);
-                player.sendMessage("");
-                player.sendMessage("§a[RGRP] §fBlockID: §3" + blockID + "§4 removed");
-                player.sendMessage("§e       - Config list updated.");
-            } else {
-                player.sendMessage("");
-                player.sendMessage("§a[RGRP] §fBlockID: §3" + blockID + "§4 is *NOT* registered §fon the list.");
-            }
-        }
-        else if (args[0].toLowerCase().charAt(0) == 'c') // check
-        {
-            if (plugin.isProtectedBlock(blockID)) {
-                player.sendMessage("");
-                player.sendMessage("§a[RGRP] §fBlockID: §3" + blockID + "§2 is registered §fon the list.");
-            } else {
-                player.sendMessage("");
-                player.sendMessage("§a[RGRP] §fBlockID: §3" + blockID + "§4 is *NOT* registered §fon the list.");
-            }
-        } else {return false;}
 
         return true;
     }
